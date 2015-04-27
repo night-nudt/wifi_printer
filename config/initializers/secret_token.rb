f849673e3f2d2e5286460b88bf00bc8682b475a2
night_nudt <night.emulifes@gmail.com>
2015-04-27 14:07:57 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WifiPrinter::Application.config.secret_key_base = 'a785a8343d9bbc43341cc8830f4432ae19e6227ce74b2047b7e239542fa1ab90cc9188187c778d7d45e5c0add15226f73857188bd97a66a38ef994578b961edd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WifiPrinter::Application.config.session_store :cookie_store, key: '_wifi_printer_session'
